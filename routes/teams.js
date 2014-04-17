96964aa2738c42dc7f3da83e226cba742c291a48
OilFanInYYC <zmunoz@gmail.com>
2014-04-17 12:08:50 -0600

    Added team.js and updated games.js

---
+++ routes/teams.js
@@ -0,0 +1,283 @@
+var teams = [
+  {
+    "name":"COLORADO AVALANCHE",
+    "TeamSchedule":"COLORADO",
+    "TeamCity":"COLORADO",
+    "TeamPXP":"COL",
+    "teamID":"COL",
+    "TeamHashtag":"#Avs",
+    "TeamNick":"AVALANCHE"
+  },
+  {
+    "name":"CHICAGO BLACKHAWKS",
+    "TeamSchedule":"CHICAGO",
+    "TeamCity":"CHICAGO",
+    "TeamPXP":"CHI",
+    "teamID":"CHI",
+    "TeamHashtag":"#Blackhawks",
+    "TeamNick":"BLACKHAWKS"
+  },
+  {
+    "name":"COLUMBUS BLUE JACKETS",
+    "TeamSchedule":"COLUMBUS",
+    "TeamCity":"COLUMBUS",
+    "TeamPXP":"CBJ",
+    "teamID":"CBJ",
+    "TeamHashtag":"#CBJ",
+    "TeamNick":"BLUE JACKETS"
+  },
+  {
+    "name":"ST. LOUIS BLUES",
+    "TeamSchedule":"ST. LOUIS",
+    "TeamCity":"ST LOUIS",
+    "TeamPXP":"STL",
+    "teamID":"STL",
+    "TeamHashtag":"#STLBlues",
+    "TeamNick":"BLUES"
+  },
+  {
+    "name":"BOSTON BRUINS",
+    "TeamSchedule":"BOSTON",
+    "TeamCity":"BOSTON",
+    "TeamPXP":"BOS",
+    "teamID":"BOS",
+    "TeamHashtag":"#Bruins",
+    "TeamNick":"BRUINS"
+  },
+  {
+    "name":"MONTREAL CANADIENS",
+    "TeamSchedule":"MONTREAL",
+    "TeamCity":"MONTREAL",
+    "TeamPXP":"MTL",
+    "teamID":"MTL",
+    "TeamHashtag":"#Habs",
+    "TeamNick":"CANADIENS"
+  },
+  {
+    "name":"VANCOUVER CANUCKS",
+    "TeamSchedule":"VANCOUVER",
+    "TeamCity":"VANCOUVER",
+    "TeamPXP":"VAN",
+    "teamID":"VAN",
+    "TeamHashtag":"#Canucks",
+    "TeamNick":"CANUCKS"
+  },
+  {
+    "name":"WASHINGTON CAPITALS",
+    "TeamSchedule":"WASHINGTON",
+    "TeamCity":"WASHINGTON",
+    "TeamPXP":"WSH",
+    "teamID":"WSH",
+    "TeamHashtag":"#Caps",
+    "TeamNick":"CAPITALS"
+  },
+  {
+    "name":"PHOENIX COYOTES",
+    "TeamSchedule":"PHOENIX",
+    "TeamCity":"PHOENIX",
+    "TeamPXP":"PHX",
+    "teamID":"PHX",
+    "TeamHashtag":"#Yotes",
+    "TeamNick":"COYOTES"
+  },
+  {
+    "name":"NEW JERSEY DEVILS",
+    "TeamSchedule":"NEW JERSEY",
+    "TeamCity":"NEW JERSEY",
+    "TeamPXP":"N.J",
+    "teamID":"NJD",
+    "TeamHashtag":"#Devils",
+    "TeamNick":"DEVILS"
+  },
+  {
+    "name":"ANAHEIM DUCKS",
+    "TeamSchedule":"ANAHEIM",
+    "TeamCity":"ANAHEIM",
+    "TeamPXP":"ANA",
+    "teamID":"ANA",
+    "TeamHashtag":"#NHLDucks",
+    "TeamNick":"DUCKS"
+  },
+  {
+    "name":"CALGARY FLAMES",
+    "TeamSchedule":"CALGARY",
+    "TeamCity":"CALGARY",
+    "TeamPXP":"CGY",
+    "teamID":"CGY",
+    "TeamHashtag":"#Flames",
+    "TeamNick":"FLAMES"
+  },
+  {
+    "name":"PHILADELPHIA FLYERS",
+    "TeamSchedule":"PHILADELPHIA",
+    "TeamCity":"PHILADELPHIA",
+    "TeamPXP":"PHI",
+    "teamID":"PHI",
+    "TeamHashtag":"#Flyers",
+    "TeamNick":"FLYERS"
+  },
+  {
+    "name":"CAROLINA HURRICANES",
+    "TeamSchedule":"CAROLINA",
+    "TeamCity":"CAROLINA",
+    "TeamPXP":"CAR",
+    "teamID":"CAR",
+    "TeamHashtag":"#NHLCanes",
+    "TeamNick":"HURRICANES"
+  },
+  {
+    "name":"NEW YORK ISLANDERS",
+    "TeamSchedule":"NY ISLANDERS",
+    "TeamCity":"NY ISLANDERS",
+    "TeamPXP":"NYI",
+    "teamID":"NYI",
+    "TeamHashtag":"#Isles",
+    "TeamNick":"ISLANDERS"
+  },
+  {
+    "name":"WINNIPEG JETS",
+    "TeamSchedule":"WINNIPEG",
+    "TeamCity":"WINNIPEG",
+    "TeamPXP":"WPG",
+    "teamID":"WPG",
+    "TeamHashtag":"#NHLJets",
+    "TeamNick":"JETS"
+  },
+  {
+    "name":"LOS ANGELES KINGS",
+    "TeamSchedule":"LOS ANGELES",
+    "TeamCity":"LOS ANGELES",
+    "TeamPXP":"L.A",
+    "teamID":"LAK",
+    "TeamHashtag":"#LAKings",
+    "TeamNick":"KINGS"
+  },
+  {
+    "name":"TAMPA BAY LIGHTNING",
+    "TeamSchedule":"TAMPA BAY",
+    "TeamCity":"TAMPA BAY",
+    "TeamPXP":"T.B",
+    "teamID":"TBL",
+    "TeamHashtag":"#TBLightning",
+    "TeamNick":"LIGHTNING"
+  },
+  {
+    "name":"TORONTO MAPLE LEAFS",
+    "TeamSchedule":"TORONTO",
+    "TeamCity":"TORONTO",
+    "TeamPXP":"TOR",
+    "teamID":"TOR",
+    "TeamHashtag":"#Leafs",
+    "TeamNick":"MAPLE LEAFS"
+  },
+  {
+    "name":"EDMONTON OILERS",
+    "TeamSchedule":"EDMONTON",
+    "TeamCity":"EDMONTON",
+    "TeamPXP":"EDM",
+    "teamID":"EDM",
+    "TeamHashtag":"#Oilers",
+    "TeamNick":"OILERS"
+  },
+  {
+    "name":"FLORIDA PANTHERS",
+    "TeamSchedule":"FLORIDA",
+    "TeamCity":"FLORIDA",
+    "TeamPXP":"FLA",
+    "teamID":"FLA",
+    "TeamHashtag":"#FLAPanthers",
+    "TeamNick":"PANTHERS"
+  },
+  {
+    "name":"PITTSBURGH PENGUINS",
+    "TeamSchedule":"PITTSBURGH",
+    "TeamCity":"PITTSBURGH",
+    "TeamPXP":"PIT",
+    "teamID":"PIT",
+    "TeamHashtag":"#Pens",
+    "TeamNick":"PENGUINS"
+  },
+  {
+    "name":"NASHVILLE PREDATORS",
+    "TeamSchedule":"NASHVILLE",
+    "TeamCity":"NASHVILLE",
+    "TeamPXP":"NSH",
+    "teamID":"NSH",
+    "TeamHashtag":"#Preds",
+    "TeamNick":"PREDATORS"
+  },
+  {
+    "name":"NEW YORK RANGERS",
+    "TeamSchedule":"NY RANGERS",
+    "TeamCity":"NY RANGERS",
+    "TeamPXP":"NYR",
+    "teamID":"NYR",
+    "TeamHashtag":"#NYR",
+    "TeamNick":"RANGERS"
+  },
+  {
+    "name":"DETROIT RED WINGS",
+    "TeamSchedule":"DETROIT",
+    "TeamCity":"DETROIT",
+    "TeamPXP":"DET",
+    "teamID":"DET",
+    "TeamHashtag":"#RedWings",
+    "TeamNick":"RED WINGS"
+  },
+  {
+    "name":"BUFFALO SABRES",
+    "TeamSchedule":"BUFFALO",
+    "TeamCity":"BUFFALO",
+    "TeamPXP":"BUF",
+    "teamID":"BUF",
+    "TeamHashtag":"#Sabres",
+    "TeamNick":"SABRES"
+  },
+  {
+    "name":"OTTAWA SENATORS",
+    "TeamSchedule":"OTTAWA",
+    "TeamCity":"OTTAWA",
+    "TeamPXP":"OTT",
+    "teamID":"OTT",
+    "TeamHashtag":"#Sens",
+    "TeamNick":"SENATORS"
+  },
+  {
+    "name":"SAN JOSE SHARKS",
+    "TeamSchedule":"SAN JOSE",
+    "TeamCity":"SAN JOSE",
+    "TeamPXP":"S.J",
+    "teamID":"SJS",
+    "TeamHashtag":"#SJSharks",
+    "TeamNick":"SHARKS"
+  },
+  {
+    "name":"DALLAS STARS",
+    "TeamSchedule":"DALLAS",
+    "TeamCity":"DALLAS",
+    "TeamPXP":"DAL",
+    "teamID":"DAL",
+    "TeamHashtag":"#DallasStars",
+    "TeamNick":"STARS"
+  },
+  {
+    "name":"ATLANTA THRASHERS",
+    "TeamSchedule":"ATLANTA",
+    "TeamCity":"ATLANTA",
+    "TeamPXP":"ATL",
+    "teamID":"ALT",
+    "TeamHashtag":"#Thrashers",
+    "TeamNick":"THRASHERS"
+  },
+  {
+    "name":"MINNESOTA WILD",
+    "TeamSchedule":"MINNESOTA",
+    "TeamCity":"MINNESOTA",
+    "TeamPXP":"MIN",
+    "teamID":"MIN",
+    "TeamHashtag":"#MNWild",
+    "TeamNick":"WILD"
+  }
+];
+
+module.exports = teams;
